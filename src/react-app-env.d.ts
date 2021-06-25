1747a9be93a9034c9a7e0d6b5efe2aa6713924a6
Krasimir Raykov <raykov@weichain.tech>
2021-06-25 18:20:56 +0300

    feat: add hydraweb3-js deps

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -25,3 +25,5 @@ declare module 'multihashes' {
   declare function decode(buff: Uint8Array): { code: number; name: string; length: number; digest: Uint8Array }
   declare function toB58String(hash: Uint8Array): string
 }
+
+declare module 'hydraweb3-js'
