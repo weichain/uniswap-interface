fb8700d021bd337b9d293438f73295bee586dcc0
Krasimir Raykov <raykov@weichain.tech>
2021-06-28 12:52:40 +0300

    feat: add useHydraProvider hook

--- src/state/hydra/hooks.ts
+++ src/state/hydra/hooks.ts
@@ -1,6 +1,23 @@
+import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { AppState } from '../index'
+import { Hydraweb3 } from 'hydraweb3-js'
 
 export function useHydraState(): AppState['hydra'] {
   return useSelector<AppState, AppState['hydra']>(state => state.hydra)
+}
+
+export function useHydraProvider(): { provider: any } {
+  const { address } = useHydraState()
+  const [provider, setProvider] = useState({})
+
+  useEffect(() => {
+    if (address) {
+      if (window.hydrawallet) {
+        setProvider(new Hydraweb3(window.hydrawallet.rpcProvider))
+      }
+    }
+  }, [address])
+
+  return { provider }
 }
